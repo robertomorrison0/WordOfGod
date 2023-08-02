cd32a99266d54e3a4ac23bb33ef1786d0ef3d3c4
robertomorrison0 <simonkrieger@t-online.de>
2023-08-02 10:55:29 +0200

    init base application

--- README.md
+++ README.md
@@ -1,23 +1,3 @@
-# GtkBible
+# bible
 
-- Very early state of a bible application (Usage at own risk, there are still memory leaks and bugs/ crashes).
-- Wtitten in C with GTK and Libadwaita.
-
-![](https://github.com/robertomorrison0/GtkBible/blob/master/preview_image.png)
-
-## How to compile
-
-Depending on your package manager you may need *-dev packages or similar to obtain the nessecary (header) files
-
-### Dependencies
-- json-c
-- libcurl
-- gtk
-- libadwaita
-
-
-```
-meson setup build
-cd build
-meson compile
-```
+A description of this project.
---
+++ src/bible-window.c
@@ -0,0 +1,50 @@
+/* bible-window.c
+ *
+ * Copyright 2023 Unknown
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ */
+
+#include "config.h"
+
+#include "bible-window.h"
+
+struct _BibleWindow
+{
+	AdwApplicationWindow  parent_instance;
+
+	/* Template widgets */
+	GtkHeaderBar        *header_bar;
+	GtkLabel            *label;
+};
+
+G_DEFINE_FINAL_TYPE (BibleWindow, bible_window, ADW_TYPE_APPLICATION_WINDOW)
+
+static void
+bible_window_class_init (BibleWindowClass *klass)
+{
+	GtkWidgetClass *widget_class = GTK_WIDGET_CLASS (klass);
+
+	gtk_widget_class_set_template_from_resource (widget_class, "/bible/robertomorrison/org/bible-window.ui");
+	gtk_widget_class_bind_template_child (widget_class, BibleWindow, header_bar);
+	gtk_widget_class_bind_template_child (widget_class, BibleWindow, label);
+}
+
+static void
+bible_window_init (BibleWindow *self)
+{
+	gtk_widget_init_template (GTK_WIDGET (self));
+}
